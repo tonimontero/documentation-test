474ff209fb734db4c21e812c2433cd77f10cb1dc
tonimontero <toni.montero@agoraimages.com>
2018-12-28 17:32:17 +0100

    initial commit

---
+++ src/test.js
@@ -0,0 +1,19 @@
+/**
+ * Multiply two numbers.
+ * @param {number} a - the first number
+ * @param {number} b - the second number
+ * @returns {number} - the product of a and b
+ */
+function multiply (a, b) {
+  return a * b
+}
+
+/**
+ * Another function.
+ * @param {number} a - the first number
+ * @param {number} b - the second number
+ * @returns - the product of a and b
+ */
+function anotherFunction (a, b) {
+  return a * b
+}
